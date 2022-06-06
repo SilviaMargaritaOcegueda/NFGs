82cdfabf7fabd7fff9b5f6cbe90f98cf75bec977
Marybeee <maria.behappy.mb@gmail.com>
2022-06-06 12:47:28 +0200

    updated Eterscan in hardhatconfig to verify contract

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
-const { API_URL_RINKEBY, PRIVATE_KEY } = process.env; 
+// to check transaction on etherscan import it
+//  it is 'like' the remix frontend and you can see all implemented functionality
+require("@nomiclabs/hardhat-etherscan");
+const { API_URL_RINKEBY, PRIVATE_KEY, API_URL_ETHERSCAN } = process.env; 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -28,4 +31,7 @@ module.exports = {
       accounts: [PRIVATE_KEY]
     }
   },
+  etherscan: {
+    apiKey: API_URL_ETHERSCAN,
+  }
 };
