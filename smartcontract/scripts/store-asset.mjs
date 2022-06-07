7dfca848b4f939f5bbbf133ae5d67fbd73149bf1
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2022-06-06 21:59:42 -0600

    Comments on the store-asset file

--- smartcontract/scripts/store-asset.mjs
+++ smartcontract/scripts/store-asset.mjs
@@ -1,3 +1,4 @@
+// We will use NFT.Storage to store our digital asset and its metadata.
 import { NFTStorage, File } from "nft.storage"
 import fs from 'fs'
 import dotenv from 'dotenv'
@@ -6,22 +7,26 @@ dotenv.config()
 const API_KEY = process.env.NFT_STORAGE_API_KEY
 
 async function storeAsset() {
-   const client = new NFTStorage({ token: API_KEY })
-   const metadata = await client.store({
-       name: 'Test NFT',
-       description: 'My Test NFT is an awesome artwork!',
-       image: new File(
-           [await fs.promises.readFile('assets/art.png')],
-           'art.png',
-           { type: 'image/png' }
-       ),
-   })
-   console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url)
+    // Create a new client connecting to NFT.Storage using the API key created.
+    const client = new NFTStorage({ token: API_KEY })
+    // Introduce the metadata consisting of name, description, and the image
+    const metadata = await client.store({
+        name: 'Test NFT',
+        description: 'My Test NFT is an awesome artwork!',
+        // Read the NFT asset directly from the file system from the assets directory
+        image: new File(
+            [await fs.promises.readFile('assets/art.png')],
+            'art.png',
+            { type: 'image/png' }
+        ),
+    })
+    //  print the metadata URL
+    console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url)
 }
 
 storeAsset()
-   .then(() => process.exit(0))
-   .catch((error) => {
-       console.error(error);
-       process.exit(1);
-   });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
