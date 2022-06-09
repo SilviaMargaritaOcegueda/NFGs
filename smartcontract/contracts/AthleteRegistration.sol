73ac45f916218c9f1420a5edfd9fb412ffd12e59
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2022-06-09 00:40:53 -0600

    athlete and club contracts adjusts

--- smartcontract/contracts/AthleteRegistration.sol
+++ smartcontract/contracts/AthleteRegistration.sol
@@ -4,21 +4,19 @@ pragma solidity ^0.8.0;
 
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-import "./Club.sol";
 
 contract AthleteRegistration is Ownable {
 
   using SafeMath for uint32;
-
-  event NewAthlete(uint athleteId, bytes32 name);
-
   
-
+  string[4] nftSorts = [ "white", "bronze", "silver", "gold" ];
+  event NewAthlete(uint athleteId, string name);
+  
   struct Athlete {
     uint athleteId;
-    bytes32 name;
+    string name;
     address athleteWallet;
-    NftSort nftSort;
+    string nftSort;
     uint32 tournaments;
     uint32 trainings;
   }
@@ -28,8 +26,9 @@ contract AthleteRegistration is Ownable {
   mapping ( address => uint) public athleteWalletToAthleteId;
   mapping (uint => Athlete) public athleteIdToAthlete;
 
-  function registerAthlete(uint _athleteId, bytes32 _name, address _athleteWallet) external onlyOwner {
-    athletes.push(Athlete(_athleteId, _name, _athleteWallet, NftSort.white, 0, 0));
+  function registerAthlete(uint _athleteId, string memory _name, address _athleteWallet) external onlyOwner {
+    Athlete memory _newAthlete = Athlete(_athleteId, _name, _athleteWallet, nftSorts[0], 0, 0);
+    athletes.push(_newAthlete);
     athleteWalletToAthleteId[_athleteWallet] = _athleteId;
     emit NewAthlete(_athleteId, _name);
   }
--- smartcontract/contracts/Club.sol
+++ smartcontract/contracts/Club.sol
@@ -4,52 +4,67 @@ pragma solidity ^0.8.0;
 
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-import "./AthleteRegistration.sol";
+import './AthleteRegistration.sol';
 
 contract Club is AthleteRegistration {
     
-    using SafeMath for uint32;
+  using SafeMath for uint32;
+  using SafeMath for uint;
 
-    uint32 trainingsPerYear;
-    uint32 pointsPerTournament;
-    enum NftSort { white, bronze, silver, gold }
+  uint32 trainingsPerYear;
+  uint32 pointsPerTournament;
+  
+  event updatedNftSort(uint athleteId, string nftSort);
 
-    mapping ( NftSort => uint32 ) private percentage;
+  mapping ( string => uint32 ) private percentage;
     
-//   address[] public whites;
-//   address[] public bronzes;
-//   address[] public silvers;
-//   address[] public goldens;
 
-    constructor(uint32 _trainingsPerYear, uint32 _pointsPerTournament) {
-      percentage[NftSort.bronze] = (>=60 <70));
-    }
+  address[] public whites;
+  address[] public bronzes;
+  address[] public silvers;
+  address[] public golds;
 
-    function resetRecords() external onlyOwner {
-      for (uint i; i < athletes.length; i++) {
-        athletes[i].tournaments = 0;
-        athletes[i].trainings = 0;
-        athletes[i].nftSort = NftSort.white;
-      }  
-    }
+  constructor(uint32 _trainingsPerYear, uint32 _pointsPerTournament) {
+    pointsPerTournament = _pointsPerTournament;
+    percentage["gold"] = _trainingsPerYear * 90 / 100;
+    percentage["silver"] = _trainingsPerYear * 70 / 100;
+    percentage["bronze"] = _trainingsPerYear * 60 / 100; 
+  }
 
-    function incrementRecords(uint _athleteId, uint32 _tournaments, uint32 _trainings) external onlyOwner {
-      athleteIdToAthlete[_athleteId].tournaments.add(_tournaments);
-      athleteIdToAthlete[_athleteId].trainings.add(_trainings);     
-    }
+  function resetRecords() external onlyOwner {
+    for (uint i; i < athletes.length; i++) {
+      athletes[i].tournaments = 0;
+      athletes[i].trainings = 0;
+      athletes[i].nftSort = nftSorts[0];
+    }  
+  }
 
-    function updateNftSort() internal onlyOwner {
-      for (uint i; i < athletes.length; i++) {
-        if {
-          athletes[i].tournaments + athletes[i].tournaments . 0;
+  function incrementRecords(uint _athleteId, uint32 _tournaments, uint32 _trainings) external onlyOwner {
+    athleteIdToAthlete[_athleteId].tournaments += _tournaments;
+    athleteIdToAthlete[_athleteId].trainings += _trainings;     
+  }
 
-        }
-          athletes[i].trainings = 0;
-          athletes[i].nftSort = NftSort.white;
-      }  
-    }
+  function updateNftSort() public onlyOwner {
+    delete whites;
+    delete bronzes;
+    delete silvers;
+    delete golds;
 
-
-
-
+    for (uint i; i < athletes.length; i++) {
+      if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["gold"]) {
+        athletes[i].nftSort = "gold";
+        golds.push(athletes[i].athleteWallet);
+      } else if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["silver"]) {
+        athletes[i].nftSort = "silver";  
+        silvers.push(athletes[i].athleteWallet);
+      } else if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["bronze"]) {
+        athletes[i].nftSort = "bronze";  
+        bronzes.push(athletes[i].athleteWallet);
+      } 
+      athletes[i].nftSort = "white";
+      whites.push(athletes[i].athleteWallet);    
+        
+      emit updatedNftSort(athletes[i].athleteId, athletes[i].nftSort);
+    }  
+  }
 }
