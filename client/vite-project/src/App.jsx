ee55015b6401a5f4c8f9e64d7255b6fcc40f2c14
Marybeee <maria.behappy.mb@gmail.com>
2022-06-10 19:43:19 +0200

    Conncetbutton

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import logo from './tennis.png'
 import './App.css'
 //import MainMint from './MainMint';
-//import NavBar from './components';
+import { NavBar, MainMint} from './components';
 
 const App = () => {
   const [accounts, setAccounts] = useState(0)
@@ -10,10 +10,13 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div>
+          <NavBar accounts={accounts} setAccounts = {setAccounts} />
+          <MainMint />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Description from Jo-Ann about our problem and our solution ;)</p>
-        {/* <NavBar accounts={accounts} setAccounts = {setAccounts} /> */}
+        {/*  */}
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
--- client/vite-project/src/components/MainMint.jsx
+++ client/vite-project/src/components/MainMint.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react'
+import { ethers, Bignumber } from 'ethers';
+//import Club from './Club.json'
+
+// which we got back when deployed contract
+const clubContratcAddress = "";
+// strating 45:12
+
+// function for BatchMint and Transfer Button
 const MainMint = () => {
+    // // check it account is connceted
+    // const isConnceted = Boolean(accounts[0]);
+
+    // async function handleMint(){
+    //     if (window.ethereum) {
+    //         co
+    //     }
+    // }
     return (
         <h1>TESTE MICH</h1>
     )
