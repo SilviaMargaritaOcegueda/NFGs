f79aece22a4a9bec7c4655dfc7480e230922c17a
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2022-06-10 16:21:54 -0600

    changes on Athlete Registration and Club contracts

--- smartcontract/contracts/Club.sol
+++ smartcontract/contracts/Club.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import './AthleteRegistration.sol';
 
 contract Club is AthleteRegistration {
@@ -11,28 +9,44 @@ contract Club is AthleteRegistration {
   using SafeMath for uint32;
   using SafeMath for uint;
 
+  // The club admin declare how many session trainings have place on a whole season.
   uint32 trainingsPerYear;
+  // The club admin declare how many extra sessions wants to assign an athlete for participating on a tournament.
   uint32 pointsPerTournament;
   
+  // Event that notifies that the NFT sorts has been 
+  // updated for all registered athletes.
   event updatedNftSort(uint athleteId, string nftSort);
 
+  // Map that holds the minimum percentaje of 
+  // attendance to get the respective NFT sort.
   mapping ( string => uint32 ) private percentage;
 
   constructor(uint32 _trainingsPerYear, uint32 _pointsPerTournament) {
     pointsPerTournament = _pointsPerTournament;
+    trainingsPerYear = _trainingsPerYear;
+    // Calculate and set the minimum of attendances to 
+    // trainning sessions that each sort of NFT implies.
+    // Gold NFT is earned when attending 90% of the sessions or more. 
     percentage["gold"] = _trainingsPerYear * 90 / 100;
+    // Silver NFT is aerned when attending 70% of the sessions or more.
     percentage["silver"] = _trainingsPerYear * 70 / 100;
+    // Bronze NFT is aerned when attending 60% of the sessions or more.
     percentage["bronze"] = _trainingsPerYear * 60 / 100; 
   }
 
+  // Reset function clears records from the last season and the NFT to white.
+  // For the time when the NFT was succesfully minted 
   function resetRecords() external onlyOwner {
     for (uint i; i < athletes.length; i++) {
+      // Delete records in the athletes array
       athletes[i].tournaments = 0;
       athletes[i].trainings = 0;
       athletes[i].nftSort = nftSorts[0];
-      athleteIdToAthlete[(athletes[i+1]).tournaments] = 0;
-      athleteIdToAthlete[(athletes[i+1]).trainings] = 0;
-      athleteIdToAthlete[(athletes[i+1]).nftSort] = nftSorts[0];
+      // Delete records in the athlete ID to Athlete mapping
+      athleteIdToAthlete[i+1].tournaments = 0;
+      athleteIdToAthlete[i+1].trainings = 0;
+      athleteIdToAthlete[i+1].nftSort = nftSorts[0];
     }  
   }
 
@@ -52,19 +66,19 @@ contract Club is AthleteRegistration {
     for (uint i; i < athletes.length; i++) {
       if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["gold"]) {
         athletes[i].nftSort = "gold";
-        athleteIdToAthlete[i+1].nftSort += "gold";
+        athleteIdToAthlete[i+1].nftSort = "gold";
         goldCounter++;
       } else if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["silver"]) {
         athletes[i].nftSort = "silver";
-        athleteIdToAthlete[i+1].nftSort += "silver"  
+        athleteIdToAthlete[i+1].nftSort = "silver";  
         silverCounter++;
       } else if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["bronze"]) {
         athletes[i].nftSort = "bronze";
-        athleteIdToAthlete[i+1].nftSort += "bronze"  
+        athleteIdToAthlete[i+1].nftSort = "bronze";  
         bronzeCounter++;
       } 
       athletes[i].nftSort = "white";
-      athleteIdToAthlete[i+1].nftSort += "white"
+      athleteIdToAthlete[i+1].nftSort = "white";
       whiteCounter++;    
         
       emit updatedNftSort(athletes[i].athleteId, athletes[i].nftSort);
