473be93a213d40cd5ca8a5f4b678fae4fdb04748
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2022-06-10 07:59:25 -0600

    Update records on both, mapping and array

--- smartcontract/contracts/Club.sol
+++ smartcontract/contracts/Club.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import './AthleteRegistration.sol';
 
 contract Club is AthleteRegistration {
@@ -17,12 +17,6 @@ contract Club is AthleteRegistration {
   event updatedNftSort(uint athleteId, string nftSort);
 
   mapping ( string => uint32 ) private percentage;
-    
-
-  address[] public whites;
-  address[] public bronzes;
-  address[] public silvers;
-  address[] public golds;
 
   constructor(uint32 _trainingsPerYear, uint32 _pointsPerTournament) {
     pointsPerTournament = _pointsPerTournament;
@@ -36,33 +30,42 @@ contract Club is AthleteRegistration {
       athletes[i].tournaments = 0;
       athletes[i].trainings = 0;
       athletes[i].nftSort = nftSorts[0];
+      athleteIdToAthlete[(athletes[i+1]).tournaments] = 0;
+      athleteIdToAthlete[(athletes[i+1]).trainings] = 0;
+      athleteIdToAthlete[(athletes[i+1]).nftSort] = nftSorts[0];
     }  
   }
 
   function incrementRecords(uint _athleteId, uint32 _tournaments, uint32 _trainings) external onlyOwner {
     athleteIdToAthlete[_athleteId].tournaments += _tournaments;
-    athleteIdToAthlete[_athleteId].trainings += _trainings;     
+    athleteIdToAthlete[_athleteId].trainings += _trainings;
+    athletes[_athleteId-1].tournaments += _tournaments;
+    athletes[_athleteId-1].trainings += _trainings;
   }
 
   function updateNftSort() public onlyOwner {
-    delete whites;
-    delete bronzes;
-    delete silvers;
-    delete golds;
+    uint32 whiteCounter;
+    uint32 bronzeCounter;
+    uint32 silverCounter;
+    uint32 goldCounter;
 
     for (uint i; i < athletes.length; i++) {
       if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["gold"]) {
         athletes[i].nftSort = "gold";
-        golds.push(athletes[i].athleteWallet);
+        athleteIdToAthlete[i+1].nftSort += "gold";
+        goldCounter++;
       } else if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["silver"]) {
-        athletes[i].nftSort = "silver";  
-        silvers.push(athletes[i].athleteWallet);
+        athletes[i].nftSort = "silver";
+        athleteIdToAthlete[i+1].nftSort += "silver"  
+        silverCounter++;
       } else if((athletes[i].tournaments.mul(pointsPerTournament) + athletes[i].tournaments) >= percentage["bronze"]) {
-        athletes[i].nftSort = "bronze";  
-        bronzes.push(athletes[i].athleteWallet);
+        athletes[i].nftSort = "bronze";
+        athleteIdToAthlete[i+1].nftSort += "bronze"  
+        bronzeCounter++;
       } 
       athletes[i].nftSort = "white";
-      whites.push(athletes[i].athleteWallet);    
+      athleteIdToAthlete[i+1].nftSort += "white"
+      whiteCounter++;    
         
       emit updatedNftSort(athletes[i].athleteId, athletes[i].nftSort);
     }  
